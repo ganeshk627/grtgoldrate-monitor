e731ace94262dd765df2e738a2e80b2b731a5584
Ganesh K <ganeshkannan627@gmail.com>
2024-09-23 10:10:45 +0530

    update

--- scrape_grtrates.py
+++ scrape_grtrates.py
@@ -21,30 +21,29 @@ def scrape_data():
         page.screenshot(path="screenshot.png", full_page=False)
         time.sleep(5)
 
-        # Scrape the data (replace with your selectors)
-        # gold24 = page.query_selector('//li[contains(text(),"GOLD - 24k -")]').inner_text()
-        # gold22 = page.query_selector('//li[contains(text(),"GOLD - 22k -")]').inner_text()
-        # gold18 = page.query_selector('//li[contains(text(),"GOLD - 18k -")]').inner_text()
-        # platinum = page.query_selector('//li[contains(text(),"PLATINUM -")]').inner_text()
-        # silver = page.query_selector('//li[contains(text(),"SILVER -")]').inner_text()
-        #
-        # subject = f'GRT Gold Rate Today - {datetime.now().strftime("%d/%m/%Y")}'
-        # body = f'\n\t{gold24}\n\t{gold22}\n\t{gold18}\n\t{platinum}\n\t{silver}'
-        # email_config.body = body
-        # print(subject, body)
-        # send_mail(body=body, subject=subject, screenshots=None)
-        #
-        # gold24price = re.search(r'Rs(\d+)', gold24).group(1)
-        # gold22price = re.search(r'Rs(\d+)', gold22).group(1)
-        # gold18price = re.search(r'Rs(\d+)', gold18).group(1)
-        # platinumprice = re.search(r'Rs(\d+)', platinum).group(1)
-        # silverprice = re.search(r'Rs(\d+)', silver).group(1)
-        #
-        #
-        # # Save data to CSV
-        # with open('grt_rates.csv', mode='a', newline='') as file:
-        #     writer = csv.writer(file)
-        #     writer.writerow([datetime.now(), gold24price, gold22price, gold18price, platinumprice, silverprice])
+        gold24 = page.query_selector('//li[contains(text(),"GOLD - 24k -")]').inner_text()
+        gold22 = page.query_selector('//li[contains(text(),"GOLD - 22k -")]').inner_text()
+        gold18 = page.query_selector('//li[contains(text(),"GOLD - 18k -")]').inner_text()
+        platinum = page.query_selector('//li[contains(text(),"PLATINUM -")]').inner_text()
+        silver = page.query_selector('//li[contains(text(),"SILVER -")]').inner_text()
+
+        subject = f'GRT Gold Rate Today - {datetime.now().strftime("%d/%m/%Y")}'
+        body = f'\n\t{gold24}\n\t{gold22}\n\t{gold18}\n\t{platinum}\n\t{silver}'
+        email_config.body = body
+        print(subject, body)
+        send_mail(body=body, subject=subject, screenshots=None)
+
+        gold24price = re.search(r'Rs(\d+)', gold24).group(1)
+        gold22price = re.search(r'Rs(\d+)', gold22).group(1)
+        gold18price = re.search(r'Rs(\d+)', gold18).group(1)
+        platinumprice = re.search(r'Rs(\d+)', platinum).group(1)
+        silverprice = re.search(r'Rs(\d+)', silver).group(1)
+
+
+        # Save data to CSV
+        with open('grt_rates.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([datetime.now(), gold24price, gold22price, gold18price, platinumprice, silverprice])
 
         browser.close()
 
