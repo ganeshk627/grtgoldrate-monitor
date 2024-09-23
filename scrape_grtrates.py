c4b770bb06386a275d621beb1c12ccbebadb9aaf
Ganesh K <ganeshkannan627@gmail.com>
2024-09-23 10:16:34 +0530

    update

--- scrape_grtrates.py
+++ scrape_grtrates.py
@@ -7,13 +7,19 @@ import re
 
 import email_config
 from send_email import  send_mail
+import json
 
 
 # Function to scrape data
 def scrape_data():
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
+
+        with open('cookies.json', 'r') as f:
+            cookies = json.load(f)
+            page.context.add_cookies(cookies)
+
         page.set_viewport_size({"width": 1600, "height": 1200})
 
         # Visit website (replace URL with your target site)
@@ -26,6 +32,10 @@ def scrape_data():
         gold18 = page.query_selector('//li[contains(text(),"GOLD - 18k -")]').inner_text()
         platinum = page.query_selector('//li[contains(text(),"PLATINUM -")]').inner_text()
         silver = page.query_selector('//li[contains(text(),"SILVER -")]').inner_text()
+
+        # cookies = page.context.cookies()
+        # with open('cookies.json', 'w') as f:
+        #     json.dump(cookies, f)
 
         subject = f'GRT Gold Rate Today - {datetime.now().strftime("%d/%m/%Y")}'
         body = f'\n\t{gold24}\n\t{gold22}\n\t{gold18}\n\t{platinum}\n\t{silver}'
